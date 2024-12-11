9674266b36765ecbac159bac0815c8ea40a64939
NaTAzOx <kevcondamin@gmail.com>
2024-12-11 13:53:25 +0100

    Working on december11

--- December11/december11.cpp
+++ December11/december11.cpp
@@ -18,6 +18,43 @@ vector<string> split(string& str, string& pattern) {
   return tokens;
 }
 
+int countDigits(int number) {
+  if (number == 0) {
+    return 1;
+  }
+  
+  int count = 0;
+  
+  while (number != 0) {
+    number = number / 10;
+    count++;
+  }
+  
+  return count;
+}
+
+vector<int> process(vector<int> previousNumbers) {
+  vector<int> result;
+  for (auto number : previousNumbers) {
+    int digits = countDigits(number);
+    
+    if (digits % 2 == 0) {
+      string numberToSplit = to_string(number);
+      int firstHalf = stoi(numberToSplit.substr(0, numberToSplit.length() / 2));
+      int secondHalf = stoi(numberToSplit.substr(numberToSplit.length() / 2));
+      result.push_back(firstHalf);
+      result.push_back(secondHalf);
+    } else {
+      if (number == 0) {
+        result.push_back(1);
+      } else {
+        result.push_back(number * 2024);
+      }
+    }
+  }
+  return result;
+}
+
 int main() {
 	ifstream file("input");
 	string str;
@@ -31,6 +68,11 @@ int main() {
 	  number = stoi(numChar);
 	  cout << number << endl;
 	  numbers.push_back(number);
+	}
+	
+	while (occurrence <= 25) {
+	  numbers = process(numbers);
+	  occurrence++;
 	}
 	
 	cout << numbers.size() << endl;
